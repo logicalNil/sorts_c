b5acf5b3cba2902d20782a880b1ba5d7c409fced
Nilay <nilayranjan99@gmail.com>
2022-07-14 12:40:51 +0530

    12:40

---
+++ src/main/kotlin/add.kt
@@ -0,0 +1,34 @@
+// kotlin code to make ladder of h height and w width
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val reader = Scanner(System.`in`)
+    print("Enter two numbers: ")
+
+    // nextDouble() reads the next double from the keyboard
+    val first = reader.nextDouble()
+    val second = reader.nextDouble()
+
+    print("Enter an operator (+, -, *, /): ")
+    val operator = reader.next()[0]
+
+    val result: Double
+
+    when (operator) {
+        '+' -> result = first + second
+        '-' -> result = first - second
+        '*' -> result = first * second
+        '/' -> result = first / second
+        // operator doesn't match any case constant (+, -, *, /)
+        else -> {
+            System.out.printf("Error! operator is not correct")
+            return
+        }
+    }
+
+    System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result)
+}
+
+
+
